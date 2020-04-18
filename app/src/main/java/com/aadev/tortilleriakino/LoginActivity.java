ad7b11bad8752dcbb7a425c71b8d6e8b979ec9c3
David Hernandez <davidh.c001606@gmail.com>
2020-04-18 00:25:04 -0700

    18/04/2020 00:24

--- app/src/main/java/com/aadev/tortilleriakino/LoginActivity.java
+++ app/src/main/java/com/aadev/tortilleriakino/LoginActivity.java
@@ -1,19 +1,15 @@
 package com.aadev.tortilleriakino;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,7 +17,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout emailInputEditText, passwordInputEditText;
-    private EditText correo, contra;
     private FirebaseAuth mAuth;
 
     @Override
