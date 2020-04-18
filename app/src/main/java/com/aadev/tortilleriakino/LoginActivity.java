94bef69e34f59296229da5dcf7e1739f7f4fc5df
David Hernandez <davidh.c001606@gmail.com>
2020-04-17 17:23:28 -0700

    17/04/2020 17:02

---
+++ app/src/main/java/com/aadev/tortilleriakino/LoginActivity.java
@@ -0,0 +1,69 @@
+package com.aadev.tortilleriakino;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private EditText correo, contra;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.user_login);
+        contra = findViewById(R.id.password_login);
+        Button nextBtn = findViewById(R.id.next_btn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String correoStr, contrasenia;
+                correoStr = correo.getText().toString();
+                contrasenia = contra.getText().toString();
+                signIn(correoStr, contrasenia);
+            }
+        });
+
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                            Intent main = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(main);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
